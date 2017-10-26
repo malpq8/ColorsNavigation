2a6203ee50b241e722ae0bcbc6d24e41691d4efa
malpq8 <31357365+malpq8@users.noreply.github.com>
2017-10-26 14:00:10 -0500

    Add files via upload

---
+++ Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Mike Louissaint on 10/26/17.
+//  Copyright © 2017 Mike Louissaint. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor 
+}
